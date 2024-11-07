df1e80289325547633c4081026baca5f53e2d0b7
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-11-07 13:03:31 +0400

    Initial Commit

---
+++ AiCalorie/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AiCalorie
+//
+//  Created by Mariam Joglidze on 07/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
